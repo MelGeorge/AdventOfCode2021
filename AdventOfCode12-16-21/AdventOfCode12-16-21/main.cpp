22627ca0c99ef1fbb34630e7b3447a3c370964f9
MelGeorge <mmgeorg@umich.edu>
2021-12-18 22:50:42 -0800

    16 pt 2

--- AdventOfCode12-16-21/AdventOfCode12-16-21/main.cpp
+++ AdventOfCode12-16-21/AdventOfCode12-16-21/main.cpp
@@ -12,31 +12,35 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <functional>
+#include <numeric>
 
 using namespace std;
-unsigned long VERSION_SUM = 0;
+unsigned long long VERSION_SUM = 0;
 
-void readInput();
+unsigned long long parseEncodedString(string const& encoded, int& bitsParsed);
 string convertToBinary(string hex);
-void part1();
 
 int main(int argc, const char * argv[]) {
-    readInput();
-    part1();
-    return 0;
+    ifstream infile("input.txt");
+    string encoded;
+    
+    infile >> encoded;
+    
+    int bitsParsed = 0;
+    unsigned long long result = parseEncodedString(convertToBinary(encoded), bitsParsed);
+    cout << "Version sum: " << VERSION_SUM << endl;
+    cout << "Result: " << result << endl;
 }
 
-int parseEncodedString(string const& encoded)
+unsigned long long parseEncodedString(string const& encoded, int& bitsParsed)
 {
-    unsigned long version = strtoul(encoded.substr(0, 3).c_str(), nullptr, 2);
-    unsigned long typeID = strtoul(encoded.substr(3, 3).c_str(), nullptr, 2);
-    
-    //cout << "version: " << version << endl;
-    //cout << "typeID: " << typeID << endl;
+    unsigned long long version = strtoull(encoded.substr(0, 3).c_str(), nullptr, 2);
+    unsigned long long typeID = strtoull(encoded.substr(3, 3).c_str(), nullptr, 2);
     
     VERSION_SUM += version;
     
-    int bitsParsed = 6;
+    bitsParsed += 6;
     
     if(typeID == 4) // packet is a literal value.
     {
@@ -49,49 +53,89 @@ int parseEncodedString(string const& encoded)
         
         value += encoded.substr(bitsParsed, 5).substr(1);
         bitsParsed += 5;
-        //cout << "value: " << strtoul(value.c_str(), nullptr, 2) << endl;
+        
+        return strtoull(value.c_str(), nullptr, 2);
     }
-    else // packet is an "operator."
+    
+    // packet is an "operator."
+    vector<unsigned long long> subs;
+    
+    if (encoded[bitsParsed] == '0')
     {
-        if (encoded[bitsParsed] == '0')
+        unsigned long long expectedBits =
+            strtoull(encoded.substr(7, 15).c_str(), nullptr, 2);
+        bitsParsed += 16;
+        int subpacketBits = 0;
+        while(subpacketBits < expectedBits)
         {
-            unsigned long expectedBits = strtoul(encoded.substr(7, 15).c_str(), nullptr, 2);
-            //cout << "expected bits: " << expectedBits << endl;
-            bitsParsed += 16;
-            int subpacketBits = 0;
-            while(subpacketBits < expectedBits)
-            {
-                int parsed = parseEncodedString(encoded.substr(bitsParsed));
-                subpacketBits += parsed;
-                bitsParsed += parsed;
-            }
+            int parsed = 0;
+            subs.push_back(
+                      parseEncodedString(encoded.substr(bitsParsed), parsed));
+            subpacketBits += parsed;
+            bitsParsed += parsed;
         }
-        else if(encoded[bitsParsed] == '1')
+    }
+    else if(encoded[bitsParsed] == '1')
+    {
+        unsigned long long expectedPackets =
+            strtoull(encoded.substr(7, 11).c_str(), nullptr, 2);
+        bitsParsed += 12;
+        while(subs.size() < expectedPackets)
         {
-            unsigned long expectedPackets = strtoul(encoded.substr(7, 11).c_str(), nullptr, 2);
-            //cout << "expected packets: " << expectedPackets << endl;
-            bitsParsed += 12;
-            int subpackets = 0;
-            while(subpackets < expectedPackets)
-            {
-                subpackets++;
-                bitsParsed += parseEncodedString(encoded.substr(bitsParsed));
-            }
+            int parsed = 0;
+            subs.push_back(
+                      parseEncodedString(encoded.substr(bitsParsed), parsed));
+            bitsParsed += parsed;
         }
     }
     
-    return bitsParsed;
-}
-
-void readInput()
-{
-    ifstream infile("input.txt");
-    string encoded;
+    unsigned long long result = 0;
     
-    infile >> encoded;
+    switch (typeID)
+    {
+        case 0: // sum
+        {
+            for(auto s : subs)
+                result += s;
+            break;
+        }
+        case 1: // product
+        {
+            result = 1;
+            for(auto s : subs)
+                result *= s;
+            break;
+        }
+        case 2: // min
+        {
+            result = *min_element(subs.begin(), subs.end());
+            break;
+        }
+        case 3: // max
+        {
+            result = *max_element(subs.begin(), subs.end());
+            break;
+        }
+        case 5: // >
+        {
+            result = (unsigned long long)(subs[0] > subs[1]);
+            break;
+        }
+        case 6: // <
+        {
+            result = (unsigned long long)(subs[0] < subs[1]);
+            break;
+        }
+        case 7: // ==
+        {
+            result = (unsigned long long)(subs[0] == subs[1]);
+            break;
+        }
+        default:
+            break;
+    }
     
-    parseEncodedString(convertToBinary(encoded));
-    cout << "Version sum: " << VERSION_SUM << endl;
+    return result;
 }
 
 string convertToBinary(string hex)
@@ -102,56 +146,23 @@ string convertToBinary(string hex)
     {
         switch (c)
         {
-            case '0':
-                binary += "0000";
-                break;
-            case '1':
-                binary += "0001";
-                break;
-            case '2':
-                binary += "0010";
-                break;
-            case '3':
-                binary += "0011";
-                break;
-            case '4':
-                binary += "0100";
-                break;
-            case '5':
-                binary += "0101";
-                break;
-            case '6':
-                binary += "0110";
-                break;
-            case '7':
-                binary += "0111";
-                break;
-            case '8':
-                binary += "1000";
-                break;
-            case '9':
-                binary += "1001";
-                break;
-            case 'A':
-                binary += "1010";
-                break;
-            case 'B':
-                binary += "1011";
-                break;
-            case 'C':
-                binary += "1100";
-                break;
-            case 'D':
-                binary += "1101";
-                break;
-            case 'E':
-                binary += "1110";
-                break;
-            case 'F':
-                binary += "1111";
-                break;
-            default:
-                break;
+            case '0': binary += "0000"; break;
+            case '1': binary += "0001"; break;
+            case '2': binary += "0010"; break;
+            case '3': binary += "0011"; break;
+            case '4': binary += "0100"; break;
+            case '5': binary += "0101"; break;
+            case '6': binary += "0110"; break;
+            case '7': binary += "0111"; break;
+            case '8': binary += "1000"; break;
+            case '9': binary += "1001"; break;
+            case 'A': binary += "1010"; break;
+            case 'B': binary += "1011"; break;
+            case 'C': binary += "1100"; break;
+            case 'D': binary += "1101"; break;
+            case 'E': binary += "1110"; break;
+            case 'F': binary += "1111"; break;
+            default: break;
         }
     }
     
